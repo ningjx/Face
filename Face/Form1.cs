58c3197763e5d7dbfb4dad947cc4bee26d31ec80
wangning <wangning@beisen.com>
2019-03-14 10:40:40 +0800

    添加IronPython和Redis,Redis基础配置

--- Face/Form1.cs
+++ Face/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Face
+namespace RedisConfig
 {
     public partial class Form1 : Form
     {
--- Face/Program.cs
+++ Face/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Face
+namespace RedisConfig
 {
     static class Program
     {
